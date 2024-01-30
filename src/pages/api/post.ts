77e8b37870747bfe874fc8035cd345ed7d9e5cc8
Nicholas Molnar <65710+neekolas@users.noreply.github.com>
2024-01-30 12:00:38 -0800

    Make XMTP friendly

--- src/pages/api/post.ts
+++ src/pages/api/post.ts
@@ -1,52 +1,69 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from "next";
 
-import { BASE_URL, generateFarcasterFrame, validateMessage } from '@/utils'
+import {
+  BASE_URL,
+  generateFarcasterFrame,
+  validateMessage,
+  validateXmtpMessage,
+} from "@/utils";
+import { FramePostPayload } from "@xmtp/frames-validator/dist/src/types";
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  if (req.method !== 'POST') {
-    res.status(405).json({ error: 'Method Not Allowed' })
-    return
+  if (req.method !== "POST") {
+    res.status(405).json({ error: "Method Not Allowed" });
+    return;
   }
 
-  const signedMessage = req.body as {
-    untrustedData: {
-      fid: number
-      url: string
-      messageHash: string
-      timestamp: number
-      network: number
-      buttonIndex: number
-      castId: { fid: number; hash: string }
-    }
-    trustedData?: {
-      messageBytes: string
-    }
-  }
+  const signedMessage = req.body as
+    | {
+        untrustedData: {
+          fid: number;
+          url: string;
+          messageHash: string;
+          timestamp: number;
+          network: number;
+          buttonIndex: number;
+          castId: { fid: number; hash: string };
+        };
+        trustedData?: {
+          messageBytes: string;
+        };
+      }
+    | FramePostPayload;
 
   // `trustedData` doesn't get returned by the Warpcast embed debugger, but we should validate it if it's there
   // This if statement should probs be removed in prod
   if (signedMessage.trustedData) {
-    const isMessageValid = await validateMessage(
-      signedMessage.trustedData.messageBytes
-    )
+    if ("conversationIdentifier" in signedMessage.untrustedData) {
+      try {
+        await validateXmtpMessage(signedMessage as FramePostPayload);
+      } catch (e) {
+        console.error(e);
+        return res.status(400).json({ error: "Invalid message" });
+      }
+    } else {
+      const isMessageValid = await validateMessage(
+        signedMessage.trustedData.messageBytes
+      );
 
-    if (!isMessageValid) {
-      return res.status(400).json({ error: 'Invalid message' })
+      if (!isMessageValid) {
+        return res.status(400).json({ error: "Invalid message" });
+      }
     }
   }
 
-  const choice = signedMessage.untrustedData.buttonIndex
+  const choice = signedMessage.untrustedData.buttonIndex;
 
-  let html: string = ''
+  let html: string = "";
 
   if (choice === 1) {
-    html = generateFarcasterFrame(`${BASE_URL}/happy.jpg`, choice)
+    html = generateFarcasterFrame(`${BASE_URL}/happy.jpg`, choice);
   } else {
-    html = generateFarcasterFrame(`${BASE_URL}/threat.jpg`, choice)
+    html = generateFarcasterFrame(`${BASE_URL}/threat.jpg`, choice);
   }
 
-  return res.status(200).setHeader('Content-Type', 'text/html').send(html)
+  return res.status(200).setHeader("Content-Type", "text/html").send(html);
 }
--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,8 @@
-import { getHubRpcClient, Message } from '@farcaster/hub-web'
+import { getHubRpcClient, Message } from "@farcaster/hub-web";
+import { validateFramesPost } from "@xmtp/frames-validator";
+import { FramePostPayload } from "@xmtp/frames-validator/dist/src/types";
 
-export const BASE_URL = process.env.BASE_URL
+export const BASE_URL = process.env.BASE_URL;
 
 // generate an html page with the relevant opengraph tags
 export function generateFarcasterFrame(image: string, choice: number) {
@@ -15,22 +17,29 @@ export function generateFarcasterFrame(image: string, choice: number) {
       ${
         choice === 2
           ? '<meta property="fc:frame:button:1" content="Yes" />'
-          : ''
+          : ""
       }
     </head>
     <body>
       
     </body>
     </html>
-  `
+  `;
+}
+
+export async function validateXmtpMessage(body: FramePostPayload) {
+  if (body.untrustedData.walletAddress) {
+    const data = await validateFramesPost(body);
+    return data?.verifiedWalletAddress;
+  }
 }
 
 export async function validateMessage(messageBytes: string) {
-  const client = getHubRpcClient('https://nemes.farcaster.xyz:2283', {})
-  const hubMessage = Message.decode(Buffer.from(messageBytes, 'hex'))
-  const res = await client.validateMessage(hubMessage)
+  const client = getHubRpcClient("https://nemes.farcaster.xyz:2283", {});
+  const hubMessage = Message.decode(Buffer.from(messageBytes, "hex"));
+  const res = await client.validateMessage(hubMessage);
 
   if (res.isOk() && res.value.valid) {
-    return res.value.valid
+    return res.value.valid;
   }
 }
